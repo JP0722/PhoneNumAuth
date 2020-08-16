95b6b89115d298a984f552b3413b2e9e6faff8c2
JP0722 <pallaprkash07@gmail.com>
2020-08-16 19:51:51 +0530

    commit-1

--- app/src/main/java/com/example/phonenumberauth/MainActivity.java
+++ app/src/main/java/com/example/phonenumberauth/MainActivity.java
@@ -19,8 +19,11 @@ import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.concurrent.TimeUnit;
 
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText phone_number,password,string1,string2;
     Button sign_in_but,sign_up_but,add_to_fb;
+    DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReference("BasicUserData");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +46,48 @@ public class MainActivity extends AppCompatActivity {
         sign_in_but.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                final String fin_phno="+91"+phone_number.getText().toString();
+
+               databaseReference.orderByChild("phoneNumber").equalTo(fin_phno).addListenerForSingleValueEvent(new ValueEventListener() {
+                   @Override
+                   public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                       if(snapshot.exists())
+                       {
+                          DataSnapshot data= snapshot.getChildren().iterator().next();
+
+
+                           String passwordFb=data.child("password").getValue().toString();
+                            if(passwordFb.equals(password.getText().toString()))
+                            {
+                              String uRandGenId=data.child("id").getValue().toString();
+                              String userName=data.child("name").getValue().toString();
+                              Toast.makeText(MainActivity.this, "SIGNED IN SUCCESSFULLY", Toast.LENGTH_SHORT).show();
+                              Intent login_int=new Intent(MainActivity.this,HomePage.class);
+                              login_int.putExtra("uRandGenId",uRandGenId);
+                              login_int.putExtra("phoneNumber",fin_phno);
+                              login_int.putExtra("userName",userName);
+
+                              startActivity(login_int);
+                            }
+                            else
+                            {
+                                Toast.makeText(MainActivity.this, "WRONG PASSWORD, TRY AGAIN", Toast.LENGTH_SHORT).show();
+                            }
+                       }
+                       else
+                       {
+                           Toast.makeText(MainActivity.this, "PHONE NUMBER DOESN'T EXIST IN OUR DATABASE", Toast.LENGTH_SHORT).show();
+                       }
+
+                   }
+
+                   @Override
+                   public void onCancelled(@NonNull DatabaseError error) {
+
+                   }
+               });
 
             }
         });
